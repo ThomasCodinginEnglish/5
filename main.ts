3a8f4bc1772c84d6e97c9543231cb3d9f2234522
Thomas Coding <thomascodinginenglish@gmail.com>
2025-04-10 21:31:23 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,6 +4,10 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Asleep)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.stopAllSounds()
+})
+music.setVolume(15)
 basic.forever(function () {
     music.play(music.stringPlayable("C5 B A G F E D C ", 269), music.PlaybackMode.UntilDone)
 })
